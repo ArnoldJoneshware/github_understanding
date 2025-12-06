4861c79527e45ebcf0de69ef7afbddd4b64c4bb9
=Arnold Joneshware <arnold@Arnold.local>
2025-12-06 21:11:47 +0530

    My first commit by addinga main and additio  file

---
+++ main.py
@@ -0,0 +1,17 @@
+from addition import addition
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second numeber : "))
+
+added_Result = addition(a , b)
+
+print(f"the addition of {a} and {b} is {added_Result}")
+
+
+
+
+# if __name__ == "__main__":
+
+#     main()
+
+# if __name__ == "__main__":
+#     main()
